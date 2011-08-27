27ba2bb2166eb1e8b05726065ac99569143f9536
kcembrey@gmail.com <kcembrey@gmail.com@a1d57dba-bcfb-cac3-e4fd-2b53bd0c2ebe>
2011-08-27 21:05:15 +0000

    Stuff

--- trunk/mainMenu.lua
+++ trunk/mainMenu.lua
@@ -2,28 +2,46 @@ module(..., package.seeall)
 function new()
 	local self = display.newGroup()
 	
-	local startButtonPress = function( event )
+	local classicButtonPress = function( event )
 		local backgroundMusic = audio.loadStream("backgroundMusic.mp3")
-		local backgroundMusicChannel = audio.play( backgroundMusic, { channel=1, loops=-1, fadein=5000 }  )  -- play the background music on channel 1, loop infinitely, and fadein over 5 seconds 
-		director:changeScene("timedModeMain", "moveFromRight")
+		--local backgroundMusicChannel = audio.play( backgroundMusic, { channel=1, loops=-1, fadein=5000 }  )  -- play the background music on channel 1, loop infinitely, and fadein over 5 seconds 
+		director:changeScene("characterSelect", "moveFromRight", {"superlaunch"})
+	end
+	
+	local levelButtonPress = function( event )
+		local backgroundMusic = audio.loadStream("backgroundMusic.mp3")
+		--local backgroundMusicChannel = audio.play( backgroundMusic, { channel=1, loops=-1, fadein=5000 }  )  -- play the background music on channel 1, loop infinitely, and fadein over 5 seconds 
+		director:changeScene("characterSelect", "moveFromRight", {"levelMenu"})
 	end
 	
 	local highScoresButtonPress = function( event )
 		director:changeScene("highScores", "moveFromTop")
 	end
 	
-	local startButton = ui.newButton{
+	local classicButton = ui.newButton{
 				default = "buttonRed.png",
 				over = "buttonRedOver.png",
-				onPress = startButtonPress,
-				text = "Start",
+				onPress = classicButtonPress,
+				text = "Classic Play",
 				emboss = true,
 				x = 240,
 				y = 55
 			}
-	startButton.isVisible = true
+	classicButton.isVisible = true	
+	self:insert(classicButton)
 	
-	self:insert(startButton)
+	local levelButton = ui.newButton{
+				default = "buttonRed.png",
+				over = "buttonRedOver.png",
+				onPress = levelButtonPress,
+				text = "LevelPlay",
+				emboss = true,
+				x = 240,
+				y = 155
+			}
+	levelButton.isVisible = true
+	self:insert(levelButton)
+	
 	local highScoresButton = ui.newButton{
 				default = "buttonRed.png",
 				over = "buttonRedOver.png",
@@ -35,6 +53,35 @@ function new()
 			}
 	highScoresButton.isVisible = true
 	self:insert(highScoresButton)
+	
+	
+
+	-- Handler that gets notified when the alert closes
+	local function onComplete( event )
+			if "clicked" == event.action then
+					local i = event.index
+					if 1 == i then							
+						os.exit()
+					elseif 2 == i then
+						-- Do nothing; dialog will simply dismiss
+					end
+			end
+	end
+	 
+	
+	-- Key listener
+	local function onKeyEvent( event )
+		local phase = event.phase
+		local keyName = event.keyName
+		
+		-- Show alert with five buttons
+		local alert = native.showAlert( "Corona", "Are you sure you want to exit?", 
+										{ "YES", "NO" }, onComplete )
+		return true
+	end
+	
+	-- Add the key callback
+	Runtime:addEventListener( "back", onKeyEvent );
 	
 	function clean()
 	end
