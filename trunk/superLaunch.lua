93916b55b8c96bc13ac7a15392d468fc9634b859
kcembrey@gmail.com <kcembrey@gmail.com@a1d57dba-bcfb-cac3-e4fd-2b53bd0c2ebe>
2011-12-15 23:08:12 +0000

    Incorporated trial for issue 1: Background objects needed for speed judgement

--- trunk/superLaunch.lua
+++ trunk/superLaunch.lua
@@ -208,6 +208,23 @@ function new( arguments )
 			game:insert( ttsky2 )
 			ttsky2:setReferencePoint( display.CenterLeftReferencePoint )
 			ttsky2.x = startingSkyX2; ttsky2.y = -820
+			
+			mountain_big = display.newImage("mountain_big.png", 132-240, 92) 
+			mountain_big2 = display.newImage("mountain_big.png", 132-720, 92) 
+			mountain_sma = display.newImage("mountain_small.png", 84, 111)
+			mountain_sma2 = display.newImage("mountain_small.png", 84 - 480, 111)
+			 
+			tree_s = display.newImage("tree_s.png", 129-30, 151) 
+			tree_s2 = display.newImage("tree_s.png", 270 + 10,151)
+			tree_l = display.newImage("tree_l.png", 145, 131) 
+			 
+			tree_s3 = display.newImage("tree_s.png", 129-30 - 320, 151) 
+			tree_s4 = display.newImage("tree_s.png", 270 + 10 - 320,151)
+			tree_l2 = display.newImage("tree_l.png", 145 - 320, 131) 
+			 
+			tree_s5 = display.newImage("tree_s.png", 129 - 30 - 640, 151) 
+			tree_s6 = display.newImage("tree_s.png", 270 + 10 - 640,151)
+			tree_l3 = display.newImage("tree_l.png", 145 - 640, 131)
 
 			local grass = display.newImage( "grass.png", true )
 			game:insert( grass )
@@ -693,6 +710,64 @@ function new( arguments )
 					ttsky.x = sky.x
 					ttsky2.x = sky2.x
 				end	
+				
+				if game.y < 320 then
+					mountain_big.x = mountain_big.x - vx
+					if mountain_big.x < -480 - mountain_big.width / 2 then
+					mountain_big:translate(480*2 , 0)
+					end
+					mountain_big2.x = mountain_big2.x - vx
+					if mountain_big2.x < -480 - mountain_big2.width / 2 then
+							mountain_big2:translate(480*2 , 0)
+					end
+					mountain_sma.x = mountain_sma.x - vx
+					if mountain_sma.x < -480 - mountain_sma.width / 2 then
+							mountain_sma:translate(480*2,0)
+					end
+					mountain_sma2.x = mountain_sma2.x - vx
+					if mountain_sma2.x < -480 - mountain_sma2.width / 2 then
+							mountain_sma2:translate(480*2,0)
+					end
+					
+					tree_s.x = tree_s.x - ( vx * 1.3 )
+					if tree_s.x < -480 - tree_s.width / 2 then
+							tree_s:translate(480*2 , 0)
+					end
+					tree_s2.x = tree_s2.x - ( vx * 1.3 )
+					if tree_s2.x < -480 - tree_s2.width / 2 then
+							tree_s2:translate(480*2 , 0)
+					end
+					tree_l.x = tree_l.x - ( vx * 1.3 )
+					if tree_l.x < -480 - tree_l.width / 2 then
+							tree_l:translate(480*2 , 0)
+					end
+					
+					tree_s3.x = tree_s3.x - ( vx * 1.6 )
+					if tree_s3.x < -480 - tree_s3.width / 2 then
+							tree_s3:translate(480*2 , 0)
+					end
+					tree_s4.x = tree_s4.x - ( vx * 1.6 )
+					if tree_s4.x < -480 - tree_s4.width / 2 then
+							tree_s4:translate(480*2 , 0)
+					end
+					tree_l2.x = tree_l2.x - ( vx * 1.6 )
+					if tree_l2.x < -480 - tree_l2.width / 2 then
+							tree_l2:translate(480*2 , 0)
+					end
+					
+					tree_s5.x = tree_s5.x - ( vx * 2 )
+					if tree_s5.x < -480 - tree_s5.width / 2 then
+							tree_s5:translate(480*2 , 0)
+					end
+					tree_s6.x = tree_s6.x - ( vx * 2 )
+					if tree_s6.x < -480 - tree_s6.width / 2 then
+							tree_s6:translate(480*2 , 0)
+					end
+					tree_l3.x = tree_l3.x - ( vx * 2 )
+					if tree_l3.x < -480 - tree_l3.width / 2 then
+							tree_l3:translate(480*2 , 0)
+					end
+				end
 			end
 			
 			if life <= 0 then	
