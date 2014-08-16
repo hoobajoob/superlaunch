37dd6339e4b456e455c26fb66d4c20d3831bcdb5
kcembrey@gmail.com <kcembrey@gmail.com@a1d57dba-bcfb-cac3-e4fd-2b53bd0c2ebe>
2014-08-16 02:52:17 +0000

    More work on rope.

--- trunk/Cocos/Superlaunch/src/Rope.js
+++ trunk/Cocos/Superlaunch/src/Rope.js
@@ -3,9 +3,9 @@ var Rope = cc.Class.extend({
     shape:null,
     startJoint:null,
     endJoint:null,
-    dampening:1,
-    length:20,
-    stiffness:15,
+    dampening:20,
+    length:.5,
+    stiffness:100,
     links:[],
 
 
@@ -14,7 +14,7 @@ var Rope = cc.Class.extend({
      * @param {cp.Space *}
      * @param {cc.p}
      */
-    ctor:function (spriteSheet, space, length, startBody, endBody) {
+    ctor:function (spriteSheet, space, length, startBody, endBody, startPoint) {
         this.space = space;
         var sprite = cc.PhysicsSprite.create(res.chainlink_png);
         // init physics
@@ -23,13 +23,14 @@ var Rope = cc.Class.extend({
 
         var body = new cp.Body(1, cp.momentForBox(1, width, height));
         sprite.setBody(body);
+        body.p = startPoint;
         spriteSheet.addChild(sprite);
         this.space.addBody(body);
 
         var zeroPoint = cc.p(0,0);
-        var leftPoint = cc.p(-width / 2,0);
-        var rightPoint = cc.p(width / 2,0);
-        this.startJoint = new cp.DampedSpring(body, startBody, leftPoint, rightPoint, this.length, this.stiffness, this.dampening);
+        var leftPoint = cc.p(-width / 3,0);
+        var rightPoint = cc.p(width / 3,0);
+        this.startJoint = new cp.DampedSpring(body, startBody, rightPoint, zeroPoint, this.length, this.stiffness, this.dampening);
         this.space.addConstraint(this.startJoint);
         spriteSheet.addChild(this.startJoint);
 
@@ -41,13 +42,13 @@ var Rope = cc.Class.extend({
             sprite.setBody(body);
             spriteSheet.addChild(sprite);
             this.space.addBody(body);
-            var midJoint = new cp.DampedSpring(body, this.links[i], zeroPoint, zeroPoint, this.length, this.stiffness, this.dampening);
+            var midJoint = new cp.DampedSpring(body, this.links[i], leftPoint, rightPoint, this.length, this.stiffness, this.dampening);
             this.space.addConstraint(midJoint);
             spriteSheet.addChild(midJoint);
             this.links.push(body);
         }
 
-        this.endJoint = new cp.DampedSpring(endBody, body, zeroPoint, zeroPoint, this.length, this.stiffness, this.dampening);
+        this.endJoint = new cp.DampedSpring(endBody, body, zeroPoint, leftPoint, this.length, this.stiffness, this.dampening);
         this.space.addConstraint(this.endJoint);
         spriteSheet.addChild(this.endJoint);
     },
