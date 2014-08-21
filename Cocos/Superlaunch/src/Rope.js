62cd8f91fcbc534077198281338f54b591db803b
kcembrey <kcembrey@gmail.com>
2014-08-21 02:50:22 +0000

    Temporary solution for launch rope

--- Cocos/Superlaunch/src/Rope.js
+++ Cocos/Superlaunch/src/Rope.js
@@ -4,7 +4,7 @@ var Rope = cc.Class.extend({
     characterJoint:null,
     slingshotJoint:null,
     dampening:10,
-    length:2,
+    length:30,
     stiffness:100,
     links:[],
     startLink:null,
@@ -18,44 +18,54 @@ var Rope = cc.Class.extend({
      */
     ctor:function (spriteSheet, space, length, characterBody, slingshotBody, startPoint) {
         this.space = space;
- /*       var sprite = cc.PhysicsSprite.create(res.chainlink_png);
-        // init physics
-        var width = sprite.getContentSize().width;
-        var height = sprite.getContentSize().height;
-
-        var body = new cp.Body(1, cp.momentForBox(1, width, height));
-        sprite.setBody(body);
-        body.p = startPoint;
-        spriteSheet.addChild(sprite);
-        this.startLink = body;
-        this.space.addBody(this.startLink);
-
-        var zeroPoint = cc.p(0,0);
-        var leftPoint = cc.p(-width / 3,0);
-        var rightPoint = cc.p(width / 3,0);
-        this.characterJoint = new cp.SlideJoint(body, characterBody, rightPoint, zeroPoint, 0, this.length);
-        body.setMoment(10000);
-        this.space.addConstraint(this.characterJoint);
-        spriteSheet.addChild(this.characterJoint);
-
-        this.links.push(body);
-        for (var i = 0; i < length; i++)
-        {
+        if (length > 0) {
             var sprite = cc.PhysicsSprite.create(res.chainlink_png);
-            this.endLink = new cp.Body(1, cp.momentForBox(1, width, height));
-            sprite.setBody(this.endLink);
-            this.endLink.p = startPoint;
-            spriteSheet.addChild(sprite);
-            this.space.addBody(this.endLink);
-            var midJoint = new cp.SlideJoint(this.endLink, this.links[i], leftPoint, rightPoint, 0, this.length);
-            this.space.addConstraint(midJoint);
-            spriteSheet.addChild(midJoint);
-            this.endLink.setMoment(10000);
-            this.links.push(this.endLink);
-        }*/
+            // init physics
+            var width = sprite.getContentSize().width;
+            var height = sprite.getContentSize().height;
 
-        this.slingshotJoint = new cp.SlideJoint(slingshotBody, characterBody, cc.p(0,0), cc.p(0,0), 0, this.length);
-        space.addConstraint(this.slingshotJoint);
+            var body = new cp.Body(1, cp.momentForBox(1, width, height));
+            sprite.setBody(body);
+            body.p = startPoint;
+            spriteSheet.addChild(sprite);
+            this.startLink = body;
+            this.space.addBody(this.startLink);
+
+            var zeroPoint = cc.p(0, 0);
+            var leftPoint = cc.p(-width / 3, 0);
+            var rightPoint = cc.p(width / 3, 0);
+            this.characterJoint = new cp.SlideJoint(body, characterBody, rightPoint, zeroPoint, 1, this.length);
+            body.setMoment(10000);
+            this.space.addConstraint(this.characterJoint);
+            spriteSheet.addChild(this.characterJoint);
+            this.endLink = this.startLink;
+
+            this.links.push(body);
+            for (var i = 1; i < length; i++) {
+                var sprite = cc.PhysicsSprite.create(res.chainlink_png);
+                this.endLink = new cp.Body(1, cp.momentForBox(1, width, height));
+                sprite.setBody(this.endLink);
+                this.endLink.p = startPoint;
+                spriteSheet.addChild(sprite);
+                this.space.addBody(this.endLink);
+                var midJoint = new cp.SlideJoint(this.endLink, this.links[i - 1], leftPoint, rightPoint, 1, this.length);
+                this.space.addConstraint(midJoint);
+                spriteSheet.addChild(midJoint);
+                this.endLink.setMoment(10000);
+                this.links.push(this.endLink);
+            }
+        }
+        else
+        {
+            this.endLink = characterBody;
+        }
+
+        this.slingshotJoint = new cp.DampedSpring(slingshotBody, this.endLink, cc.p(0,0), cc.p(0,0), this.length, this.stiffness, this.dampening);
+        if (length == 0)
+        {
+            this.characterJoint = this.slingshotJoint;
+        }
+        this.space.addConstraint(this.slingshotJoint);
         spriteSheet.addChild(this.slingshotJoint);
     },
 
